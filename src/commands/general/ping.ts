0cf1747217d312a8bd40efb929b62112bc709d2b
daniellwdb <44723767+daniellwdb@users.noreply.github.com>
2021-05-28 10:57:15 +0200

    feat(akira): delete ping command

--- src/commands/general/ping.ts
+++
@@ -1,6 +0,0 @@
-import type { Command } from "../../utilities/loadCommandsAndEvents"
-
-export const command: Command = {
-  description: "Ping",
-  execute: (message) => message.reply("🏓 Pong"),
-}
